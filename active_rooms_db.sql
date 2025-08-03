da4018aa17c328b5beba06cff6634a71772cb897
Shalev Ozeri <shalevozeri951@gmail.com>
2025-08-03 15:50:34 +0300

    client update

--- active_rooms_db.sql
+++ active_rooms_db.sql
@@ -64,7 +64,7 @@ CREATE TABLE `areas` (
 
 LOCK TABLES `areas` WRITE;
 /*!40000 ALTER TABLE `areas` DISABLE KEYS */;
-INSERT INTO `areas` VALUES ('tte644','Main Hall','Large open area for events','M10,10 L50,10 L50,50 L10,50 Z',18);
+INSERT INTO `areas` VALUES ('1','building 1','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('2','building 2','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('3','building 3','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('4','building 4','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('45','building 1','Large open area for events','M10,10 L50,10 L50,50 L10,50 Z',10),('5','building 5','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('6','building 6','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('7','building 7','Default description','M0,0 L10,0 L10,10 L0,10 Z',10),('8','building 8','Default description','M0,0 L10,0 L10,10 L0,10 Z',10);
 /*!40000 ALTER TABLE `areas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,12 +78,11 @@ DROP TABLE IF EXISTS `rooms`;
 CREATE TABLE `rooms` (
   `id` varchar(10) NOT NULL,
   `description` text,
-  `area` varchar(10) DEFAULT NULL,
+  `area` int unsigned NOT NULL,
   `x` int DEFAULT NULL,
   `y` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `area` (`area`),
-  CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`area`) REFERENCES `areas` (`id`)
+  KEY `area` (`area`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -93,8 +92,39 @@ CREATE TABLE `rooms` (
 
 LOCK TABLES `rooms` WRITE;
 /*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES ('1','Conference Room','tte644',10,20),('2','Conference Room','tte644',10,20),('4','Conference Room','tte644',10,20),('6','Conference Room','tte644',10,20);
+INSERT INTO `rooms` VALUES ('15','Conference Room',1,10,20),('2','Conference Room',2,300,150),('4','Conference Room',6,450,300),('64','Conference Room',3,10,20),('78','Conference Room',4,11,20),('90','Conference Room',5,10,20);
 /*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensors`
+--
+
+DROP TABLE IF EXISTS `sensors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sensors` (
+  `id` varchar(10) NOT NULL,
+  `room_id` varchar(10) DEFAULT NULL,
+  `x` int NOT NULL,
+  `y` int NOT NULL,
+  `status` enum('available','occupied','error') DEFAULT 'available',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `room_id` (`room_id`),
+  CONSTRAINT `sensors_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensors`
+--
+
+LOCK TABLES `sensors` WRITE;
+/*!40000 ALTER TABLE `sensors` DISABLE KEYS */;
+INSERT INTO `sensors` VALUES ('S001','15',150,100,'available','2025-08-02 22:16:08','2025-08-02 22:16:08'),('S002','2',300,150,'occupied','2025-08-02 22:16:08','2025-08-02 22:16:08'),('S003','4',450,200,'error','2025-08-02 22:16:08','2025-08-02 22:16:08'),('S004','64',200,300,'available','2025-08-02 22:16:08','2025-08-02 22:16:08'),('S007','15',150,120,'available','2025-08-03 10:39:00','2025-08-03 10:39:00'),('S008','64',250,70,'error','2025-08-03 10:57:18','2025-08-03 10:57:18');
+/*!40000 ALTER TABLE `sensors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -122,6 +152,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('U001','admin','admin123','admin@hit.ac.il','054-1234567',1),('U002','user','user123','user@hit.ac.il','054-7654321',0),('U003','manager','manager123','manager@hit.ac.il','054-9876543',1),('U004','student','student123','student@hit.ac.il','054-5555555',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -132,6 +163,10 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-08-03 14:46:44
+
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2025-07-11 13:35:41
